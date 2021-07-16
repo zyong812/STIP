1eaa57da124c52c0eac473d252d769f0785549a2
360BUYAD\zhangyong513 <yongzhang@qq.com>
2021-07-16 11:10:21 +0800

    eval reading

--- hotr/metrics/vcoco/ap_role.py
+++ hotr/metrics/vcoco/ap_role.py
@@ -25,7 +25,7 @@ class APRole(object):
         if h_box.shape[0] == 0 : return # if no prediction, just return
         # COCO (O), V-COCO (X) __or__ collater, no ann in image => ignore
 
-        valid_i_inds = (i_act[:, 0] != -1) # (n_i, )
+        valid_i_inds = (i_act[:, 0] != -1) # (n_i, ) # i_act 只是一种过滤的作用
         overlaps = compute_overlap(h_box, i_box) # (n_h, n_i)
         assigned_input = np.argmax(overlaps, axis=1) # (n_h, )
         v_inds = valid_i_inds[assigned_input] # (n_h, )
@@ -84,18 +84,18 @@ class APRole(object):
                 self.fp[label] = np.append(self.fp[label], f)
 
         else:
-            # 对于每一个 human，每一种 action 取一个 score 最高的 pair
+            # 对于每一个 human instance，每一种 action 取一个分数最高的 pair。比如 human1+hold+保留一个obj
             s_obj_argmax = np.argmax(score.reshape(-1, n_o), axis=1).reshape(-1, n_h) # (#act, n_h)
             s_obj_max = np.max(score.reshape(-1, n_o), axis=1).reshape(-1, n_h) # (#act, n_h)
 
             h_overlaps = compute_overlap(h_box, p_box[:, :4]) # (n_h, n_p)
-
+            # 怎么对图中的 human-relation-obj 标注为 match (TP) 还是不match (FP)
             for label in range(len(self.act_name)):
                 h_inds = np.argsort(s_obj_max[label])[::-1] # (n_h, )
                 self.score[label] = np.append(self.score[label], s_obj_max[label, h_inds])
 
                 p_inds = (p_act[:, label] == 1) # (n_p, )
-                if p_inds.sum() == 0:
+                if p_inds.sum() == 0: # 没有此类关系标注时，全部认为 FP
                     self.tp[label] = np.append(self.tp[label], np.array([0]*n_h))
                     self.fp[label] = np.append(self.fp[label], np.array([1]*n_h))
                     continue
@@ -106,44 +106,44 @@ class APRole(object):
                 # for same human, multiple pairs exist. find the human box that has the same idx with max overlap hbox.
                 h_max_temp = np.expand_dims(h_max_overlap, axis=1)
                 h_over_thresh = (h_overlaps == h_max_temp) # (n_h, n_p)
-                h_over_thresh = h_over_thresh & np.expand_dims(p_inds, axis=0) # (n_h, n_p) # find only for current act
+                h_over_thresh = h_over_thresh & np.expand_dims(p_inds, axis=0) # (n_h, n_p) # find only for current act -- 匹配了主语和谓语
 
                 h_valid = h_over_thresh.sum(axis=1)>0 # (n_h, ) # at least one is True
                 # h_valid -> if all is False, then argmax becomes 0. <- prevent
-                assigned_p = np.argmax(h_over_thresh, axis=1) # (n_h, ) # p only for current act
+                assigned_p = np.argmax(h_over_thresh, axis=1) # (n_h, ) # p only for current act -- 根据主语和谓语已经找到了对应哪个 GT relation
 
                 o_mapping_box = o_box[s_obj_argmax[label]][h_inds] # (n_h, ) # find where T is.
                 p_mapping_box = p_box[assigned_p, 4:8] # (n_h, 4)
 
-                o_overlaps = compute_overlap(o_mapping_box, p_mapping_box)
+                o_overlaps = compute_overlap(o_mapping_box, p_mapping_box) # 再看宾语是不是能匹配上
                 o_overlaps = np.diag(o_overlaps) # (n_h, )
                 o_overlaps.setflags(write=1)
                 if (~h_valid).sum() > 0:
                     o_overlaps[~h_valid] = 0 # (n_h, )
 
-                no_role_inds = (p_box[assigned_p, 4] == -1) # (n_h, )
-                nan_box_inds = np.all(o_mapping_box == 0, axis=1) | np.all(np.isnan(o_mapping_box), axis=1)
+                no_role_inds = (p_box[assigned_p, 4] == -1) # (n_h, ) -- GT relation obj box 为空
+                nan_box_inds = np.all(o_mapping_box == 0, axis=1) | np.all(np.isnan(o_mapping_box), axis=1) ## -- predicted relation obj box 为 0 or nan
                 no_role_inds = no_role_inds & h_valid
                 nan_box_inds = nan_box_inds & h_valid
 
                 h_iou_inds = (h_max_overlap > self.iou_threshold) # (n_h, )
                 o_iou_inds = (o_overlaps > self.iou_threshold) # (n_h, )
 
-                if self.scenario_flag: # scenario_1
+                if self.scenario_flag: # scenario_1: 对于 GT relation obj box 为空 的情况，预测的 obj box 也要准确
                     o_iou_inds[no_role_inds & nan_box_inds] = 1
                     o_iou_inds[no_role_inds & ~nan_box_inds] = 0
-                else: # scenario_2
+                else: # scenario_2: 对于 GT relation obj box 为空 的情况，不考虑预测的 obj box，都认为是正确
                     o_iou_inds[no_role_inds] = 1
 
                 iou_inds = (h_iou_inds & o_iou_inds)
                 p_nonzero = iou_inds.nonzero()[0]
                 p_inds = assigned_p[p_nonzero]
-                p_iou = np.unique(p_inds, return_index=True)[1]
+                p_iou = np.unique(p_inds, return_index=True)[1] # 去重，一个GT最多匹配一次
                 p_tp = p_nonzero[p_iou]
 
                 t = np.zeros(n_h, dtype=np.uint8)
-                t[p_tp] = 1
-                f = 1-t
+                t[p_tp] = 1 # 最终匹配了的设为 TP
+                f = 1-t # 否则设为 FP
 
                 self.tp[label] = np.append(self.tp[label], t)
                 self.fp[label] = np.append(self.fp[label], f)
@@ -156,7 +156,7 @@ class APRole(object):
 
             # sort by score
             indices = np.argsort(-self.score[label])
-            self.fp[label] = self.fp[label][indices]
+            self.fp[label] = self.fp[label][indices] # 所有图片的 predicted relations 一起排序计算 AP
             self.tp[label] = self.tp[label][indices]
 
 
