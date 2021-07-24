2c350866181086e038de79d9b2d2a5359ba04a21
360BUYAD\zhangyong513 <yongzhang@qq.com>
2021-07-24 21:49:05 +0800

    minor

--- hotr/engine/arg_parser.py
+++ hotr/engine/arg_parser.py
@@ -123,7 +123,7 @@ def get_args_parser():
     parser.add_argument('--resume', default='', help='resume from checkpoint')
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                         help='start epoch')
-    parser.add_argument('--num_workers', default=2, type=int)
+    parser.add_argument('--num_workers', default=4, type=int)
 
     # mode
     parser.add_argument('--eval', action='store_true', help="Only evaluate results if the flag is provided")
